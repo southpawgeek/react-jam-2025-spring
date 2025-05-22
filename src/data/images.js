c321438e069e6a4411a0fc4d6190a05509a34e07
Jen Guerra <jen@southpawgeek.com>
2025-05-22 18:55:35 -0400

    add images

--- src/data/images.js
+++ src/data/images.js
@@ -1,43 +1,47 @@
+import npcAsuka from "../assets/npc-public-asuka.png"
+import npcBeardKing from "../assets/npc-public-bk.png"
 import npcBiggy from "../assets/npc-public-biggy.png"
+import npcBloopsly from "../assets/npc-public-bloopsly.png"
 import npcCapsly from "../assets/npc-public-capsly.png"
 import npcDotti from "../assets/npc-public-dotti.png"
-import npcFab from "../assets/npc-public-fab.png"
-import npcFred from "../assets/npc-public-fred.png"
-import npcWonda from "../assets/npc-public-wonda.png"
-import npcBeardKing from "../assets/npc-public-bk.png"
-import npcWinry from "../assets/npc-public-winry.png"
-import npcOscar from "../assets/npc-public-oscar.png"
-import npcAsuka from "../assets/npc-public-asuka.png"
-import npcBloopsly from "../assets/npc-public-bloopsly.png"
 import npcEliza from "../assets/npc-public-eliza.png"
+import npcFab from "../assets/npc-public-fab.png"
+import npcFaina from "../assets/npc-public-faina.png"
+import npcFred from "../assets/npc-public-fred.png"
+import npcLalana from "../assets/npc-public-lalana.png"
+import npcOscar from "../assets/npc-public-oscar.png"
 import npcTashi from "../assets/npc-public-tashi.png"
-import bgCoffee from "../assets/bg-coffee-shop.png"
-import bgRussia from "../assets/bg-russia.png"
-import bgTrain from "../assets/bg-train-station.png"
+import npcWinry from "../assets/npc-public-winry.png"
+import npcWonda from "../assets/npc-public-wonda.png"
 import bgBeach from "../assets/bg-beach.png"
 import bgCastle from "../assets/bg-castle.png"
+import bgCoffee from "../assets/bg-coffee-shop.png"
 import bgOffice from "../assets/bg-office.png"
+import bgRussia from "../assets/bg-russia.png"
+import bgTrain from "../assets/bg-train-station.png"
 
 const images = {
-  biggy: { src: npcBiggy, scale: 0.09 },
-  capsly: { src: npcCapsly, scale: 0.09 },
-  dotti: { src: npcDotti, scale: 0.09 },
-  fab: { src: npcFab, scale: 0.09 },
-  fred: { src: npcFred, scale: 0.09 },
-  wonda: { src: npcWonda, scale: 0.09 },
-  bk: { src: npcBeardKing, scale: 0.09 },
-  winry: { src: npcWinry, scale: 0.09 },
-  oscar: { src: npcOscar, scale: 0.09 },
-  asuka: { src: npcAsuka, scale: 0.09 },
-  bloopsly: { src: npcBloopsly, scale: 0.09 },
-  eliza: { src: npcEliza, scale: 0.09 },
-  tashi: { src: npcTashi, scale: 0.09 },
-  bgCoffee: { src: bgCoffee, scale: 0.5 },
-  bgRussia: { src: bgRussia, scale: 0.5 },
-  bgTrain: { src: bgTrain, scale: 0.5 },
   bgBeach: { src: bgBeach, scale: 0.5 },
   bgCastle: { src: bgCastle, scale: 0.5 },
+  bgCoffee: { src: bgCoffee, scale: 0.5 },
   bgOffice: { src: bgOffice, scale: 0.5 },
+  bgRussia: { src: bgRussia, scale: 0.5 },
+  bgTrain: { src: bgTrain, scale: 0.5 },
+  asuka: { src: npcAsuka, scale: 0.09 },
+  biggy: { src: npcBiggy, scale: 0.09 },
+  bk: { src: npcBeardKing, scale: 0.09 },
+  bloopsly: { src: npcBloopsly, scale: 0.09 },
+  capsly: { src: npcCapsly, scale: 0.09 },
+  dotti: { src: npcDotti, scale: 0.09 },
+  eliza: { src: npcEliza, scale: 0.09 },
+  fab: { src: npcFab, scale: 0.09 },
+  faina: { src: npcFaina, scale: 0.09 },
+  fred: { src: npcFred, scale: 0.09 },
+  lalana: { src: npcLalana, scale: 0.09 },
+  oscar: { src: npcOscar, scale: 0.09 },
+  tashi: { src: npcTashi, scale: 0.09 },
+  winry: { src: npcWinry, scale: 0.09 },
+  wonda: { src: npcWonda, scale: 0.09 },
 }
 
 export default images
